a066596ebad7cd30ac9b663e88b9e45a25381e51
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-19 18:36:29 +0600

    Factorial Using For loop

--- factorial.js
+++ factorial.js
@@ -3,17 +3,13 @@
 3! = (3-1)!X3
 4! = (4-1)!X4
 5! = (5-1)!X5
+5! = 1X2X3X4X5
 6! = (6-1)!X6
 n! = (n-1)!Xn
 */
-
-function factorila(range){
-    var start = 1,fact=1;
-    while(start <= range){
-        fact = fact * start;
-        start++;
-    }
-    return fact;
+var n = 5;
+let fact = 1
+for(var i=1; i<=n; i++){
+   fact = i*fact
 }
-
-console.log(factorila(5));
+console.log(fact);
