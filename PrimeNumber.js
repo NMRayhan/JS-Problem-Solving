58267187c98897c676333e1d0336bfc92f563b2b
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-20 00:34:58 +0600

    Random Number using ceil and Random Math Function

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,8 +1,4 @@
 //Swaping Number Without Temp
 
-let a=50,b=800;
-console.log("Before Swapping a:"+a+" and b:"+b);
-a=a+b;
-b=a-b;
-a=a-b;
-console.log("After Swapping a:"+a+" and b:"+b);
+var random = Math.ceil(Math.random()*10)
+console.log(random);
