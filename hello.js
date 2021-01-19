5dfd09b0045dca6e7ec983bf45e6dbb1604cbe90
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-19 16:50:06 +0600

    JS Problem Solving

---
+++ hello.js
@@ -0,0 +1,85 @@
+// var Celsius=60, Fahrenheit=140;
+
+// var Celsius1 = Celsius = ((Fahrenheit-32)*5)/9
+// var Fahrenheit1 = Fahrenheit = (Celsius*9)/(Fahrenheit-(32*5))
+
+// console.log(Celsius1);
+// console.log(Fahrenheit1);
+
+// var Name = "Nur"
+// var number = 120
+// var n = this.Name = number;
+// console.log(n);
+
+/*
+var namef = "Write a JavaScript program to get the website URL (loading page)" 
+var n = namef.split('R').pop()
+console.log(n);
+console.log(namef);
+
+var fileName = "hello.js"
+console.log(fileName.split('.').pop());
+
+function diff(n){
+    if(n < 13){
+        return 13 - n;
+    }else{
+        return (n - 13)*2
+    }
+}
+console.log(diff(2));
+console.log(diff(32));
+
+function diff(n,m){
+    var result = n+m
+    if(n == m){
+        result = result *3;
+        return result;
+    }else{
+        return result;
+    }
+}
+console.log(diff(10,10));
+
+function convert(inch){
+    return inch/12;
+}
+console.log(convert(17));
+
+function factorial(num2) {
+    let factResult = 1, num1 = 1;
+    while (num1 <= num2) {
+        factResult = num1 * factResult;
+        num1++
+    }
+    return factResult;
+}
+console.log(factorial(5));
+
+function factorial(fact){
+    if(fact == 0){
+        return 1;
+    }else{
+        return fact * factorial(fact -1);
+    }
+}
+console.log(factorial(5));
+
+let n = 1;
+function fact(num) {
+    for (let i = 1; i <= num; i++) {
+        n = i * n;
+    }
+    return n;
+}
+console.log(fact(5));
+
+2! = 1X2
+3! = (3-1)!X3
+4! = (4-1)!X4
+5! = (5-1)!X5
+6! = (6-1)!X6
+n! = (n-1)!Xn
+*/
+
+
