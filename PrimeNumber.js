aa90a7054c13fa9364931fc4b9a74688bc701034
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-20 00:38:57 +0600

    Math Function Using abs(),Floor(),ceil(),round()

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,4 +1,9 @@
 //Swaping Number Without Temp
 
-var random = Math.ceil(Math.random()*10)
-console.log(random);
+var number = -2.149;
+console.log(Math.abs(number));
+console.log(Math.round(number));
+var result = Math.ceil(number)
+console.log(result);
+var result2 = Math.floor(number)
+console.log(result2);
