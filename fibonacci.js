d9eccfe24f7e6a28ab051e52f8d045ee8ea06c4c
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-19 20:20:32 +0600

    Fibonacci Series Usign Recursive Function and Loop

--- fibonacci.js
+++ fibonacci.js
@@ -1,12 +1,21 @@
 /*
-ewnfpwo
+2 = (2-1)+(2-2)
+3 = (3-1)+(3-2)
+4 = (4-1)+(4-2)
 */
 
-function fibonacci(n){
-    var arr=[0,1];
-    for(var i=2; i<=n; i++){
-        arr[i] = arr[i-1] + arr[i-2];
+function fibonacci(n) {
+    var n1 = 0;
+    var n2 = 1;
+    if (n == 0) {
+        return n1;
+    } else if (n == 1) {
+        return n2;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
-    return arr
 }
-console.log(fibonacci(10));
+var nTerm = 10;
+for (var i = 0; i <= nTerm; i++) {
+    console.log(fibonacci(i));
+}
