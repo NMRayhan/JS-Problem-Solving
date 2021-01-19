94f904c4d659ae8907cbbef58b825573f66b60a0
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-19 19:36:41 +0600

    Fibonacci Series using Function

--- fibonacci.js
+++ fibonacci.js
@@ -2,9 +2,11 @@
 ewnfpwo
 */
 
-var arr=[0,1];
-var n = 10;
-for(var i=2; i<=n; i++){
-    arr[i] = arr[i-1] + arr[i-2];
+function fibonacci(n){
+    var arr=[0,1];
+    for(var i=2; i<=n; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    return arr
 }
-console.log(arr);
+console.log(fibonacci(10));
