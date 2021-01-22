9fef050ade50a9e6aea832030ccaf5c19e3dd124
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-22 20:43:15 +0600

    Sum of Arrau

---
+++ array.js
@@ -0,0 +1,7 @@
+var MarkArray = [1,2,3,4,5,6,7,8,9,10]
+var sum = MarkArray[0]
+for(var i=0; i<MarkArray.length; i++){
+    var element = MarkArray[i]
+     var sum = sum + element
+}
+console.log("Total of this Number Sum",sum);
