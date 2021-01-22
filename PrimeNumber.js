46135058b385da0ae93c6e9d8ee50308b77def8a
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-22 15:45:34 +0600

    MAx Number usign Math.max function

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -2,10 +2,4 @@ var number1 = 230;
 var number2 = 450;
 var number3 = 303;
 
-if(number1>number2 && number1>number3){
-    console.log("Number 1 is Bigger Then Number 2 AND Number 3");
-}else if(number2>number1 && number2>number3){
-    console.log("Number 2 is Bigger Then Number 1 AND Number 3");
-}else{
-    console.log("Number 3 is Bigger Then Number 1 AND Number 2");
-}
+console.log(Math.max(number1,number2,number3));
