3f21f572516d4ba6ceea382dc2254e18e622c653
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-23 01:16:26 +0600

    Duplicate Number Remove in Array

--- array.js
+++ array.js
@@ -1,13 +1,11 @@
-var friends = ["Alu", "Bulu", "culu", "dulu", "fulu"];
+var number = [1,2,3,4,5,3,45,3,2,7,67,3,22,3,3,13]
+var duplicate = []
 
-var allNames = "";
-
-for(var i = 0; i< friends.length;i++){
-
-    var name = friends[i];
-
-    allNames = allNames + name;
-
+for(var i=0; i<number.length; i++){
+    var element = number[i]
+    var index = duplicate.indexOf(element)
+    if(index == -1){
+        duplicate.push(element)
+    }
 }
-
-console.log(allNames)
+console.log(duplicate);
