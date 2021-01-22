9ec1699ff3b5137231e6b4b2a941b4e9c13519fb
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-22 15:41:43 +0600

    Max Number finding using if else condition

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,9 +1,11 @@
-//Swaping Number Without Temp
+var number1 = 230;
+var number2 = 450;
+var number3 = 303;
 
-var number = -2.149;
-console.log(Math.abs(number));
-console.log(Math.round(number));
-var result = Math.ceil(number)
-console.log(result);
-var result2 = Math.floor(number)
-console.log(result2);
+if(number1>number2 && number1>number3){
+    console.log("Number 1 is Bigger Then Number 2 AND Number 3");
+}else if(number2>number1 && number2>number3){
+    console.log("Number 2 is Bigger Then Number 1 AND Number 3");
+}else{
+    console.log("Number 3 is Bigger Then Number 1 AND Number 2");
+}
