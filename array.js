8862e033848fb0c27b9facc6aa0d24b503305671
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-23 01:53:05 +0600

    Total Charecter Count in String and White Space count

--- array.js
+++ array.js
@@ -2,17 +2,14 @@ var speech = "I am hardworking. I am serious. I am sure I will do it"
 var whiteSpace = 0,charecter=0;
 for(var count=0; count<speech.length; count++){
     var char = speech[count]
-    // if(char == " "){
-    //     whiteSpace++;
-    // }else{
-    //     charecter++;
-    // }
-    if(char == "a"){
-        whiteSpace++
+    if(char == " "){
+        whiteSpace++;
+    }else{
+        charecter++;
     }
 }
-// var result = charecter - whiteSpace;
-// console.log(result);
+var result = charecter - whiteSpace;
+console.log(result);
 console.log(speech.length);
 console.log(whiteSpace);
 console.log(charecter);
