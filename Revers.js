d52dfa6e3693a40df1effb97a1101619af203a77
NMRayhan <nurmdrayhan97@gmail.com>
2021-01-23 03:02:44 +0600

    Reverse String

--- Revers.js
+++ Revers.js
@@ -1,10 +1,11 @@
 function stringRevers(str){
-    var spliteStr = str.split("")
+    var spliteStr = str.split('')
     console.log(spliteStr);
     var reversArray = spliteStr.reverse();
     console.log(reversArray);
-    var joinArray = reversArray.join();
+    var joinArray = reversArray.join('');
     console.log(joinArray);
+    return joinArray
 }
 
 console.log(stringRevers("hello"));
